ca4b6230b69dc2f918145e833d63d202021c02b0
kareem alaa <ka054336@gmail.com>
2021-11-13 04:04:02 +0200

    offset printing

---
+++ offset/arabic/arabic.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+    // carousel
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+    
+});
+
